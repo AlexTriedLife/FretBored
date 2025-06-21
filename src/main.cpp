d02df80c6d4093beab778b3885cfe650238345c9
AlexTriedLife <alexmcphersonuribe@gmail.com>
2025-06-21 17:50:46 -0400

    Added minor scale

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,16 @@
 int main(int argc, const char * argv[]) {
     std::cout << "Working directory: " << std::filesystem::current_path() << std::endl;
     Scale major = Scale(R"(data/scales/major.json)");
+    Scale minor = Scale(R"(data/scales/minor.json)");
     std::string root = "C#";
-    std::vector<std::string> notes = major.getNotes(root);
-    for (const std::string& note : notes) {
+    std::vector<std::string> majorNotes = major.getNotes(root);
+    std::vector<std::string> minorNotes = minor.getNotes(root);
+    std::cout << root << " " << major.getName() << " scale:" << std::endl;
+    for (const std::string& note : majorNotes) {
+        std::cout << note << "\n";
+    }
+    std::cout << root << " " << minor.getName() << " scale:" << std::endl;
+    for (const std::string& note : minorNotes) {
         std::cout << note << "\n";
     }
 
